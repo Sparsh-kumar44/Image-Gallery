fb7b834a720002037526f70285d9231b2f8102d4
Sparsh-kumar44 <sparsh.kumar1412@gmail.com>
2026-03-12 00:01:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll(".card img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+const closeBtn = document.getElementById("closeBtn");
+
+cards.forEach(img => {
+img.addEventListener("click", () => {
+lightbox.style.display = "flex";
+lightboxImg.src = img.src;
+});
+});
+
+closeBtn.addEventListener("click", () => {
+lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+if(e.target !== lightboxImg){
+lightbox.style.display = "none";
+}
+});
